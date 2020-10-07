083a5edb2309e6aa2409115408d3e90d86556b6a
RealMarkSussex <marksussex6@gmail.com>
2020-10-07 11:32:20 +0100

    gets top 5 players by points per game

--- server/main.py
+++ server/main.py
@@ -3,17 +3,36 @@ import aiohttp
 import asyncio
 
 
-async def getPlayer():
+def position_converter(position):
+    position_map = {
+        1: "Goalkeeper",
+        2: "Defender",
+        3: "Midfielder",
+        4: "Forward"
+    }
+    return position_map[position]
+
+
+def player_is_better(player):
+    return player.points_per_game
+
+
+async def get_player(position, price):
     session = aiohttp.ClientSession()
     fpl = FPL(session)
 
-    await session.close()
+    players = await fpl.get_players()
+    matchingPlayers = []
 
-    players = await fpl.get_players();
-    print()
     for player in players:
-        if player.first_name == "Patrick" and player.second_name == "Bamford":
-            print(player)
-     
+        if position_converter(player.element_type) == position and player.now_cost <= price:
+            matchingPlayers.append(player)
+    await session.close()
+    
+    return matchingPlayers
 
-asyncio.run(getPlayer())
+players = asyncio.run(get_player("Defender", 57))
+players.sort(key=player_is_better, reverse=True)
+print(dir(players[0]))
+for player in players[:5]:
+    print(player)
